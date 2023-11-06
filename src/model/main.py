fc7c3ce86dbb116570af51378475277757091d7d
esayad1 <134289084+esayad1@users.noreply.github.com>
2023-11-06 20:39:26 +0100

    Add files via upload

--- src/model/main.py
+++ src/model/main.py
@@ -6,22 +6,41 @@ from imblearn.over_sampling import SMOTE
 from imblearn.pipeline import make_pipeline as make_imb_pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
+from sklearn.multiclass import OneVsRestClassifier
 from xgboost import XGBClassifier
 
-stop_words_list = stopwords.words('english') + stopwords.words('french')
+
+
 def make_model():
     return Pipeline([
         ("count_vectorizer", CountVectorizer()),
         ("random_forest", RandomForestClassifier()),
     ])
 
+stop_words_list = stopwords.words('english') + stopwords.words('french')
 
-def make_model2():
-   return make_imb_pipeline(
-       TfidfVectorizer(max_features=5000, stop_words=stop_words_list),
-       SMOTE(random_state=42),
-       LogisticRegression(max_iter=1000, random_state=42)
-   )
+def make_model2(task):
+    if task == "is_comic_video":
+       return make_imb_pipeline(
+           TfidfVectorizer(max_features=5000, stop_words=stop_words_list),
+           SMOTE(random_state=42),
+           LogisticRegression(max_iter=1000, random_state=42)
+       )
+    elif task == "is_name":
+        return make_imb_pipeline(
+            RandomForestClassifier(random_state=42)
+        )
+    elif task == "find_comic_name":
+        return make_imb_pipeline(
+            RandomForestClassifier(random_state=42)
+        )
+
+
+def make_model4():
+    return make_imb_pipeline(
+        RandomForestClassifier(random_state=42)
+    )
+
 
 
 
